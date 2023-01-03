4b9e51b393c5bce6c6310e8c4d1451ecc5540476
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-01-03 21:37:03 +0300

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,7 +3,14 @@
 
 #include <stdlib.h>
 
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * 
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 	int n;
