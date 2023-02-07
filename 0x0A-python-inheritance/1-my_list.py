8931a14c7d1933dac3795c254c7d2423c61ef60c
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-02-07 20:28:48 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
