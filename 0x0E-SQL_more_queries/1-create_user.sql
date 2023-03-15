073805bf2a100a453075dd93fa538f1a59bbbef0
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-15 17:12:38 +0300

    Create 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Creates a MySQL Server user `user_0d_1`
+-- and sets a temporary password `user_0d_1_pwd`.
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
