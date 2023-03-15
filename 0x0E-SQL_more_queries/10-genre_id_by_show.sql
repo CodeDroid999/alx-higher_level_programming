81cdc8901400667bdbbf1f9b05525b7918431a71
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-15 17:17:38 +0300

    Create 10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in `hbtn_0d_tvshows`
+-- that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
