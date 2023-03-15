bc29a99935b31369d9d7233f32818fbd3f769651
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-15 17:13:10 +0300

    Create 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Creates the database `hbtn_0d_2` and
+-- the user `user_0d_2` with specific privileges.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
